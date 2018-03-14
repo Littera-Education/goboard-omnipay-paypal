911052ef0bacf8e53ba4451fc05d348f0e38af46
Alper Hasan <alperhasan@gmail.com>
2018-03-14 10:25:04 +0200

    PayPal balance - GET request

--- src/Message/RestBalanceRequest.php
+++ src/Message/RestBalanceRequest.php
@@ -14,18 +14,25 @@ class RestBalanceRequest extends AbstractRestRequest
     public function getData()
     {
         $data = $this->getParameters();
-
+/*
         unset ($data["clientId"]);
         unset ($data["secret"]);
         unset ($data["token"]);
         unset ($data["testMode"]);
-
+*/
         return $data;
     }
 
+	protected function getHttpMethod()
+    {
+        return 'GET';
+    }
+	
     public function getEndpoint()
     {
         return parent::getEndpoint() . '/wallet/balance-accounts';
+		//$base = $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
+        //return $base . '/v1/wallet/balance-accounts';
     }
     
 }
